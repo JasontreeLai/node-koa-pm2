2822c33a484a38fd9a270c46ca59bda8281ceeb0
Jason <3271674820@qq.com>
2021-06-08 19:56:31 +0800

    publish

---
+++ app.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+const router = require('./api/test');
+
+app.use(async (ctx,next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.request.method} - ${ctx.url} - ${ms}ms`)
+})
+
+app.use(router.routes());
+
+app.listen(3000,() => {
+    console.log('服务器已启动')
+})
